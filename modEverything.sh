c36b37c62e9ebbb8885aa607649d2fffc5367334
Angelo Carrabba <acarrabba6@gmail.com>
2018-04-16 09:30:12 -0400

    added printing of what is being modded to modEverything.sh

--- modEverything.sh
+++ modEverything.sh
@@ -1,2 +1,16 @@
-chmod o+r index.html $(find ./public -type f)
-chmod u+r $(find ./api)
+function mod_read() {
+    for file in $@; do
+	echo chmod o+r $file
+	chmod o+r $file
+    done
+}
+function mod_run() {
+    for file in $@; do
+	echo chmod u+r $file
+	chmod u+r $file
+    done
+}
+
+
+mod_read index.html $(find ./public -type f)
+mod_run $(find ./api)
